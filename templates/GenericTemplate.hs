45c5ac12fb777fdc4a53ad9a13c9fe182df96ef5
Luke Iannini <lukexi@me.com>
2015-12-21 03:46:45 -0800

    Update project to Alex 3. Add a stack.yaml

--- templates/GenericTemplate.hs
+++ templates/GenericTemplate.hs
@@ -1,28 +1,3 @@
-{-# LINE 1 "templates/GenericTemplate.hs" #-}
-{-# LINE 1 "templates/GenericTemplate.hs" #-}
-{-# LINE 1 "<built-in>" #-}
-{-# LINE 17 "<built-in>" #-}
-{-# LINE 1 "/usr/local/ghc-head/lib/ghc-7.10.2/include/ghcversion.h" #-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{-# LINE 18 "<built-in>" #-}
-{-# LINE 1 "templates/GenericTemplate.hs" #-}
 -- -----------------------------------------------------------------------------
 -- ALEX TEMPLATE
 --
@@ -32,51 +7,75 @@
 -- -----------------------------------------------------------------------------
 -- INTERNALS and main scanner engine
 
-
-{-# LINE 21 "templates/GenericTemplate.hs" #-}
-
-
-
-
-
--- Do not remove this comment. Required to fix CPP parsing when using GCC and a clang-compiled alex.
-#if __GLASGOW_HASKELL__ > 706
-#define GTE(n,m) (tagToEnum# (n >=# m))
-#define EQ(n,m) (tagToEnum# (n ==# m))
-#else
-#define GTE(n,m) (n >=# m)
-#define EQ(n,m) (n ==# m)
+#ifdef ALEX_GHC
+#undef __GLASGOW_HASKELL__
+#define ALEX_IF_GHC_GT_500 #if __GLASGOW_HASKELL__ > 500
+#define ALEX_IF_GHC_LT_503 #if __GLASGOW_HASKELL__ < 503
+#define ALEX_IF_GHC_GT_706 #if __GLASGOW_HASKELL__ > 706
+#define ALEX_ELIF_GHC_500 #elif __GLASGOW_HASKELL__ == 500
+#define ALEX_IF_BIGENDIAN #ifdef WORDS_BIGENDIAN
+#define ALEX_ELSE #else
+#define ALEX_ENDIF #endif
+#define ALEX_DEFINE #define
 #endif
 
-{-# LINE 51 "templates/GenericTemplate.hs" #-}
+#ifdef ALEX_GHC
+#define ILIT(n) n#
+#define IBOX(n) (I# (n))
+#define FAST_INT Int#
+-- Do not remove this comment. Required to fix CPP parsing when using GCC and a clang-compiled alex.
+ALEX_IF_GHC_GT_706
+ALEX_DEFINE GTE(n,m) (tagToEnum# (n >=# m))
+ALEX_DEFINE EQ(n,m) (tagToEnum# (n ==# m))
+ALEX_ELSE
+ALEX_DEFINE GTE(n,m) (n >=# m)
+ALEX_DEFINE EQ(n,m) (n ==# m)
+ALEX_ENDIF
+#define PLUS(n,m) (n +# m)
+#define MINUS(n,m) (n -# m)
+#define TIMES(n,m) (n *# m)
+#define NEGATE(n) (negateInt# (n))
+#define IF_GHC(x) (x)
+#else
+#define ILIT(n) (n)
+#define IBOX(n) (n)
+#define FAST_INT Int
+#define GTE(n,m) (n >= m)
+#define EQ(n,m) (n == m)
+#define PLUS(n,m) (n + m)
+#define MINUS(n,m) (n - m)
+#define TIMES(n,m) (n * m)
+#define NEGATE(n) (negate (n))
+#define IF_GHC(x)
+#endif
 
-
+#ifdef ALEX_GHC
 data AlexAddr = AlexA# Addr#
 -- Do not remove this comment. Required to fix CPP parsing when using GCC and a clang-compiled alex.
-#if __GLASGOW_HASKELL__ < 503
+ALEX_IF_GHC_LT_503
 uncheckedShiftL# = shiftL#
-#endif
+ALEX_ENDIF
 
 {-# INLINE alexIndexInt16OffAddr #-}
 alexIndexInt16OffAddr (AlexA# arr) off =
-#ifdef WORDS_BIGENDIAN
+ALEX_IF_BIGENDIAN
   narrow16Int# i
   where
         i    = word2Int# ((high `uncheckedShiftL#` 8#) `or#` low)
         high = int2Word# (ord# (indexCharOffAddr# arr (off' +# 1#)))
         low  = int2Word# (ord# (indexCharOffAddr# arr off'))
         off' = off *# 2#
-#else
+ALEX_ELSE
   indexInt16OffAddr# arr off
+ALEX_ENDIF
+#else
+alexIndexInt16OffAddr arr off = arr ! off
 #endif
 
-
-
-
-
+#ifdef ALEX_GHC
 {-# INLINE alexIndexInt32OffAddr #-}
 alexIndexInt32OffAddr (AlexA# arr) off = 
-#ifdef WORDS_BIGENDIAN
+ALEX_IF_BIGENDIAN
   narrow32Int# i
   where
    i    = word2Int# ((b3 `uncheckedShiftL#` 24#) `or#`
@@ -87,24 +86,24 @@ alexIndexInt32OffAddr (AlexA# arr) off =
    b1   = int2Word# (ord# (indexCharOffAddr# arr (off' +# 1#)))
    b0   = int2Word# (ord# (indexCharOffAddr# arr off'))
    off' = off *# 4#
-#else
+ALEX_ELSE
   indexInt32OffAddr# arr off
+ALEX_ENDIF
+#else
+alexIndexInt32OffAddr arr off = arr ! off
 #endif
 
+#ifdef ALEX_GHC
 
-
-
-
-
-#if __GLASGOW_HASKELL__ < 503
+ALEX_IF_GHC_LT_503
 quickIndex arr i = arr ! i
-#else
+ALEX_ELSE
 -- GHC >= 503, unsafeAt is available from Data.Array.Base.
 quickIndex = unsafeAt
+ALEX_ENDIF
+#else
+quickIndex arr i = arr ! i
 #endif
-
-
-
 
 -- -----------------------------------------------------------------------------
 -- Main lexing routines
@@ -116,34 +115,34 @@ data AlexReturn a
   | AlexToken  !AlexInput !Int a
 
 -- alexScan :: AlexInput -> StartCode -> AlexReturn a
-alexScan input (I# (sc))
-  = alexScanUser undefined input (I# (sc))
+alexScan input IBOX(sc)
+  = alexScanUser undefined input IBOX(sc)
 
-alexScanUser user input (I# (sc))
-  = case alex_scan_tkn user input 0# input sc AlexNone of
+alexScanUser user input IBOX(sc)
+  = case alex_scan_tkn user input ILIT(0) input sc AlexNone of
         (AlexNone, input') ->
                 case alexGetByte input of
                         Nothing -> 
-
-
-
+#ifdef ALEX_DEBUG
+                                   trace ("End of input.") $
+#endif
                                    AlexEOF
                         Just _ ->
-
-
-
+#ifdef ALEX_DEBUG
+                                   trace ("Error.") $
+#endif
                                    AlexError input'
 
         (AlexLastSkip input'' len, _) ->
-
-
-
+#ifdef ALEX_DEBUG
+                trace ("Skipping.") $ 
+#endif
                 AlexSkip input'' len
 
         (AlexLastAcc k input''' len, _) ->
-
-
-
+#ifdef ALEX_DEBUG
+                trace ("Accept.") $ 
+#endif
                 AlexToken input''' len k
 
 
@@ -153,49 +152,49 @@ alexScanUser user input (I# (sc))
 alex_scan_tkn user orig_input len input s last_acc =
   input `seq` -- strict in the input
   let 
-        new_acc = (check_accs (alex_accept `quickIndex` (I# (s))))
+        new_acc = (check_accs (alex_accept `quickIndex` IBOX(s)))
   in
   new_acc `seq`
   case alexGetByte input of
      Nothing -> (new_acc, input)
      Just (c, new_input) -> 
-
-
-
-      case fromIntegral c of { (I# (ord_c)) ->
+#ifdef ALEX_DEBUG
+      trace ("State: " ++ show IBOX(s) ++ ", char: " ++ show c) $
+#endif
+      case fromIntegral c of { IBOX(ord_c) ->
         let
                 base   = alexIndexInt32OffAddr alex_base s
-                offset = (base +# ord_c)
+                offset = PLUS(base,ord_c)
                 check  = alexIndexInt16OffAddr alex_check offset
                 
-                new_s = if GTE(offset,0#) && EQ(check,ord_c)
+                new_s = if GTE(offset,ILIT(0)) && EQ(check,ord_c)
                           then alexIndexInt16OffAddr alex_table offset
                           else alexIndexInt16OffAddr alex_deflt s
         in
         case new_s of
-            -1# -> (new_acc, input)
+            ILIT(-1) -> (new_acc, input)
                 -- on an error, we want to keep the input *before* the
                 -- character that failed, not after.
-            _ -> alex_scan_tkn user orig_input (if c < 0x80 || c >= 0xC0 then (len +# 1#) else len)
+            _ -> alex_scan_tkn user orig_input (if c < 0x80 || c >= 0xC0 then PLUS(len,ILIT(1)) else len)
                                                 -- note that the length is increased ONLY if this is the 1st byte in a char encoding)
                         new_input new_s new_acc
       }
   where
         check_accs (AlexAccNone) = last_acc
-        check_accs (AlexAcc a  ) = AlexLastAcc a input (I# (len))
-        check_accs (AlexAccSkip) = AlexLastSkip  input (I# (len))
-
+        check_accs (AlexAcc a  ) = AlexLastAcc a input IBOX(len)
+        check_accs (AlexAccSkip) = AlexLastSkip  input IBOX(len)
+#ifndef ALEX_NOPRED
         check_accs (AlexAccPred a predx rest)
-           | predx user orig_input (I# (len)) input
-           = AlexLastAcc a input (I# (len))
+           | predx user orig_input IBOX(len) input
+           = AlexLastAcc a input IBOX(len)
            | otherwise
            = check_accs rest
         check_accs (AlexAccSkipPred predx rest)
-           | predx user orig_input (I# (len)) input
-           = AlexLastSkip input (I# (len))
+           | predx user orig_input IBOX(len) input
+           = AlexLastSkip input IBOX(len)
            | otherwise
            = check_accs rest
-
+#endif
 
 data AlexLastAcc a
   = AlexNone
@@ -211,7 +210,7 @@ data AlexAcc a user
   = AlexAccNone
   | AlexAcc a
   | AlexAccSkip
-
+#ifndef ALEX_NOPRED
   | AlexAccPred a   (AlexAccPred user) (AlexAcc a user)
   | AlexAccSkipPred (AlexAccPred user) (AlexAcc a user)
 
@@ -232,11 +231,11 @@ alexPrevCharMatches f _ input _ _ = f (alexInputPrevChar input)
 alexPrevCharIsOneOf arr _ input _ _ = arr ! alexInputPrevChar input
 
 --alexRightContext :: Int -> AlexAccPred _
-alexRightContext (I# (sc)) user _ _ input = 
-     case alex_scan_tkn user input 0# input sc AlexNone of
+alexRightContext IBOX(sc) user _ _ input = 
+     case alex_scan_tkn user input ILIT(0) input sc AlexNone of
           (AlexNone, _) -> False
           _ -> True
         -- TODO: there's no need to find the longest
         -- match when checking the right context, just
         -- the first match will do.
-
+#endif
